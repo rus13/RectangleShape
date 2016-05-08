75103d0d7048d4ca7442b880a06411259cf346ea
rus13 <rus13@users.noreply.github.com>
2016-05-08 12:14:41 +0200

    update

--- algo.js
+++ algo.js
@@ -1,5 +1,8 @@
 /*
 * O(n^2) solution
+* Uses kind of sweep line algorithm, the endpoints of the rectangles are sorted in the events array
+* open_rect holds all the rectangles the sweep line is intersecting
+* To make the algorithm into O(n log n) we need to make open rect into a priority queue with getMax, delete, and add operation in O(log n)
 * */
 function shape(rect) {
     var events = [];
@@ -20,16 +23,14 @@ function shape(rect) {
             return x1 - y1;
     };
     events.sort(comp);
-    if (events.length > 0)
-        events.push([Number.MAX_VALUE, 0, 'e']);
     var res = [];
     var cur_rect = -1;
     var cur_h = 0;
+    var open_rect = {};
     for (var i = 0; i < events.length; ++i) {
         var e_x = events[i][0];
         var e_id = events[i][1];
         var e_h = rect[e_id][2];
-        //var cur_h = (cur_rect >= 0) ? cur_rect[2] : 0;
         if (e_x == rect[e_id][0]) {// rectangle starts
             if (e_h > cur_h) {
                 res.push([e_x, cur_h]);
@@ -37,20 +38,22 @@ function shape(rect) {
                 cur_rect = e_id;
                 cur_h = e_h;
             }
+            open_rect[e_id] = 1;
         }
         else{//rectangle ends
             if(e_id == cur_rect){
                 res.push([e_x, cur_h]);
                 cur_rect = -1;
                 cur_h = 0;
-                for (var j = 0; j < rect.length; ++j) {
-                    if (rect[j][0] <= e_x && rect[j][1] > e_x && rect[j][2] > cur_h){
+                for (var j in open_rect){
+                    if (rect[j][1] > e_x && rect[j][2] > cur_h){
                         cur_rect = j;
                         cur_h = rect[j][2];
                     }
                 }
                 res.push([e_x, cur_h]);
             }
+            delete open_rect[e_id];
         }
     }
     return res;
