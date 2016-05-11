7e2f0af3171ee5411c55117a7d2c81830070ccce
rus13 <rus13@users.noreply.github.com>
2016-05-11 17:47:42 +0200

    update, comments

--- algo.js
+++ algo.js
@@ -1,28 +1,34 @@
 /*
 * O(n^2) solution
-* Uses kind of sweep line algorithm, the endpoints of the rectangles are sorted in the events array
-* open_rect holds all the rectangles the sweep line is intersecting
-* To make the algorithm into O(n log n) we need to make open rect into a priority queue with getMax, delete, and add operation in O(log n)
+* Uses a sweep line algorithm, the endpoints of the rectangles are sorted in the events array.
+* We also store the currently highest rectangle.
+* open_rect holds all the rectangles the sweep line is currently intersecting
+* On a rectangle starts event we add this rectangle to open_rect. If the new rectangle is higher than the current one, then we the currently highest rect and add new points
+* On a rectangle ends event we delete this rectangle from open_rect. Also we check if the ended rectangle is the currently highest. If yes, we update the currently highest rect (use open_rect)
+* To make the algorithm into O(n log n) we need the getMax, delete, and add operation of open_rect to be in O(log n) -> Either priority queue or balanced tree. 
 * */
 function shape(rect) {
+    //create events: start and end points of rectangles, the events hold the x coordinates and the ids of the rectangles
     var events = [];
     for (var i = 0; i < rect.length; ++i) {
         events.push([rect[i][0], i]);
         events.push([rect[i][1], i]);
     }
+    //compare function
     var comp = function (x, y) {
         var x1 = x[0];
         var y1 = y[0];
         var hx = rect[x[1]][2];
         var hy = rect[y[1]][2];
-        if (x1 == y1 && hx == hy)
+        if (x1 == y1 && hx == hy) //start points before end points
             return (x1 == rect[x[1]][0]) ? -1 : 1;
-        if (x1 == y1)
+        if (x1 == y1) // hight of the rectangle
             return hy - hx;
-        else
+        else //else x coordinate
             return x1 - y1;
     };
     events.sort(comp);
+    // res contains the points
     var res = [];
     var cur_rect = -1;
     var cur_h = 0;
@@ -31,29 +37,33 @@ function shape(rect) {
         var e_x = events[i][0];
         var e_id = events[i][1];
         var e_h = rect[e_id][2];
-        if (e_x == rect[e_id][0]) {// rectangle starts
-            if (e_h > cur_h) {
+        if (e_x == rect[e_id][0]) {//rectangle starts
+            open_rect[e_id] = 1;
+            if (e_h > cur_h) { //only if the new rectangle is higher then the current highest
                 res.push([e_x, cur_h]);
                 res.push([e_x, e_h]);
                 cur_rect = e_id;
                 cur_h = e_h;
             }
-            open_rect[e_id] = 1;
         }
         else{//rectangle ends
+            delete open_rect[e_id];
             if(e_id == cur_rect){
-                res.push([e_x, cur_h]);
-                cur_rect = -1;
-                cur_h = 0;
+                var tmp_rect = -1;
+                var tmp_h = 0;
                 for (var j in open_rect){
-                    if (rect[j][1] > e_x && rect[j][2] > cur_h){
-                        cur_rect = j;
-                        cur_h = rect[j][2];
+                    if (rect[j][1] > e_x && rect[j][2] > tmp_h){
+                        tmp_rect = j;
+                        tmp_h = rect[j][2];
                     }
                 }
-                res.push([e_x, cur_h]);
+                if(cur_h != tmp_h){ // if the rectangle start at the same height dont add points
+                    res.push([e_x, cur_h]);
+                    res.push([e_x, tmp_h]);
+                }
+                cur_h = tmp_h;
+                cur_rect = tmp_rect;
             }
-            delete open_rect[e_id];
         }
     }
     return res;
